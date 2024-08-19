2000835bf6ef23d8b854eb2b8f65d9de85300d6a
NGUYEN TAN THUAN <ntthuan0106.job@gmail.com>
2024-08-20 03:19:25 +0700

    Update keydata file location

--- Terraform/modules/AzureVM/main.tf
+++ Terraform/modules/AzureVM/main.tf
@@ -76,7 +76,7 @@ resource "azurerm_virtual_machine" "main" {
   os_profile_linux_config {
     disable_password_authentication = false
     ssh_keys {
-      key_data = file("~/.ssh/new_thuan.pub")
+      key_data = file("/home/thuan/.ssh/new_thuan.pub")
       path = "/home/thuan/.ssh/authorized_keys"
     }
   }
