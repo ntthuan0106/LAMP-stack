99a31467682d56cbcf999f590a394892dd889748
NGUYEN TAN THUAN <ntthuan0106.job@gmail.com>
2024-08-20 03:55:38 +0700

    Change line 17

--- scripts/update_inventory.sh
+++ scripts/update_inventory.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define absolute paths
-TF_DIR="/home/thuan/lamp/Terraform"
-INVENTORY_FILE="/home/thuan/lamp/Ansible/inventory"
+TF_DIR="Terraform"
+INVENTORY_FILE="Ansible/inventory"
 
 # Navigate to the Terraform directory
 cd $TF_DIR
@@ -14,7 +14,7 @@ USERNAME=$(terraform output -raw vm_username)
 # Write to the inventory file
 cat > $INVENTORY_FILE <<EOL
 [azure_vm]
-$PUBLIC_IP ansible_user=$USERNAME ansible_ssh_private_key_file=/home/thuan/.ssh/new_thuan
+$PUBLIC_IP ansible_user=$USERNAME ansible_ssh_private_key_file=~/.ssh/new_thuan
 EOL
 
 echo "Inventory file updated with IP: $PUBLIC_IP and username: $USERNAME"
