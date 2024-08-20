56592a0f1264cbd10ac43979ac243ccb53a20d1f
NGUYEN TAN THUAN <ntthuan0106.job@gmail.com>
2024-08-21 02:26:36 +0700

    Modify

--- scripts/update_inventory.sh
+++ scripts/update_inventory.sh
@@ -3,14 +3,14 @@
 # Define absolute paths
 TF_DIR="Terraform"
 INVENTORY_FILE="Ansible/inventory"
-
+chmod 664 $INVENTORY_FILE
 # Navigate to the Terraform directory
 cd $TF_DIR
 
 # Load Terraform outputs
 PUBLIC_IP=$(terraform output -raw vm_public_ip)
 USERNAME=$(terraform output -raw vm_username)
-chmod 664 $INVENTORY_FILE
+cd ..
 # Write to the inventory file
 cat > $INVENTORY_FILE <<EOL
 [azure_vm]
